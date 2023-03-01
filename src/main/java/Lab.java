29f1a9dc19de6823239a5169c04b812cd7d42c2b
Wesley Edwards <wredwards94@outlook.com>
2023-03-01 15:02:39 +0000

    lab complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -24,7 +24,11 @@ public class Lab {
      * @return true if a is equal to b, false otherwise.
      */
     public boolean isEqual(int a, int b){
-        return false;
+        if(a == b) {
+            return true;
+        }else {
+            return false;
+        }
     }
 
     /**
@@ -33,7 +37,11 @@ public class Lab {
      * @return true if a is greater than b, false otherwise.
      */
     public boolean greaterThan(int a, int b){
-        return false;
+        if(a > b) {
+            return true;
+        }else {
+            return false;
+        }
     }
     /**
      * @param a the first number to be compared.
@@ -41,7 +49,11 @@ public class Lab {
      * @return true if a is less than b, false otherwise.
      */
     public boolean lessThan(int a, int b){
-        return false;
+        if(a < b) {
+            return true;
+        }else {
+            return false;
+        }
     }
     /**
      * @param a the first number to be compared.
@@ -49,7 +61,11 @@ public class Lab {
      * @return true if a is greater than or equal to b, false otherwise.
      */
     public boolean greaterThanOrEqualTo(int a, int b){
-        return false;
+        if(a >= b) {
+            return true;
+        }else {
+            return false;
+        }
     }
     /**
      * @param a the first number to be compared.
@@ -57,7 +73,11 @@ public class Lab {
      * @return true if a is less than or equal to b, false otherwise.
      */
     public boolean lessThanOrEqualTo(int a, int b){
-        return false;
+        if(a <= b) {
+            return true;
+        }else {
+            return false;
+        }
     }
 
     /**
@@ -66,6 +86,10 @@ public class Lab {
      * @return true if a is not equal to b, false otherwise.
      */
     public boolean notEqualTo(int a, int b){
-        return false;
+        if(a != b) {
+            return true;
+        }else {
+            return false;
+        }
     }
 }
